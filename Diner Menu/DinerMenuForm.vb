3ce9a88a52fbf5ecdc24590e38bde3180d13906d
Zac <zac.minidude140@Gmail.com>
2023-10-25 14:24:48 -0600

    Added Exit functionality.   Started adding sub routines for updating display.

--- Diner Menu/DinerMenuForm.vb
+++ Diner Menu/DinerMenuForm.vb
@@ -4,5 +4,25 @@
 'Diner Menu
 'https://github.com/Minidude140/DinerMenu.git
 Public Class DinerMenuForm
+    'Custom Methods
+    Sub SetDefault()
+        DailySpecialsLabel.Text = "Welcome"
+    End Sub
+    Sub DisplaySoup()
+        DailySpecialsLabel.Text = "Soup"
+    End Sub
+    Sub DisplaySalad()
+        DailySpecialsLabel.Text = "Salad"
+    End Sub
+    Sub DisplayFish()
+        DailySpecialsLabel.Text = "Fish"
+    End Sub
+    'Event Handlers
+    Private Sub DinerMenuForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        SetDefault()
+    End Sub
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
 
 End Class
