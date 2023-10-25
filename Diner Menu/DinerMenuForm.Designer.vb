a9365489fe097d15ba3c66387f14150511270108
Zac <zac.minidude140@Gmail.com>
2023-10-25 14:15:20 -0600

    Added buttons and labels to form.  Added header code.

--- Diner Menu/DinerMenuForm.Designer.vb
+++ Diner Menu/DinerMenuForm.Designer.vb
@@ -22,10 +22,91 @@ Partial Class DinerMenuForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SoupButton = New System.Windows.Forms.Button()
+        Me.SaladButton = New System.Windows.Forms.Button()
+        Me.FishButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.DinerNameLabel = New System.Windows.Forms.Label()
+        Me.DailySpecialsLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'SoupButton
+        '
+        Me.SoupButton.Location = New System.Drawing.Point(23, 383)
+        Me.SoupButton.Name = "SoupButton"
+        Me.SoupButton.Size = New System.Drawing.Size(153, 58)
+        Me.SoupButton.TabIndex = 0
+        Me.SoupButton.Text = "&Soup"
+        Me.SoupButton.UseVisualStyleBackColor = True
+        '
+        'SaladButton
+        '
+        Me.SaladButton.Location = New System.Drawing.Point(182, 383)
+        Me.SaladButton.Name = "SaladButton"
+        Me.SaladButton.Size = New System.Drawing.Size(153, 58)
+        Me.SaladButton.TabIndex = 1
+        Me.SaladButton.Text = "S&alad"
+        Me.SaladButton.UseVisualStyleBackColor = True
+        '
+        'FishButton
+        '
+        Me.FishButton.Location = New System.Drawing.Point(341, 383)
+        Me.FishButton.Name = "FishButton"
+        Me.FishButton.Size = New System.Drawing.Size(153, 58)
+        Me.FishButton.TabIndex = 2
+        Me.FishButton.Text = "&Fish"
+        Me.FishButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(643, 383)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(153, 58)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'DinerNameLabel
+        '
+        Me.DinerNameLabel.AutoSize = True
+        Me.DinerNameLabel.Font = New System.Drawing.Font("Gill Sans Ultra Bold Condensed", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DinerNameLabel.Location = New System.Drawing.Point(333, 9)
+        Me.DinerNameLabel.Name = "DinerNameLabel"
+        Me.DinerNameLabel.Size = New System.Drawing.Size(192, 46)
+        Me.DinerNameLabel.TabIndex = 4
+        Me.DinerNameLabel.Text = "Diner Name"
+        '
+        'DailySpecialsLabel
+        '
+        Me.DailySpecialsLabel.AutoSize = True
+        Me.DailySpecialsLabel.Location = New System.Drawing.Point(392, 93)
+        Me.DailySpecialsLabel.Name = "DailySpecialsLabel"
+        Me.DailySpecialsLabel.Size = New System.Drawing.Size(73, 17)
+        Me.DailySpecialsLabel.TabIndex = 5
+        Me.DailySpecialsLabel.Text = "Menu Item"
+        '
+        'DinerMenuForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(808, 453)
+        Me.Controls.Add(Me.DailySpecialsLabel)
+        Me.Controls.Add(Me.DinerNameLabel)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.FishButton)
+        Me.Controls.Add(Me.SaladButton)
+        Me.Controls.Add(Me.SoupButton)
+        Me.Name = "DinerMenuForm"
+        Me.Text = "Diner Name's Special Menu"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents SoupButton As Button
+    Friend WithEvents SaladButton As Button
+    Friend WithEvents FishButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents DinerNameLabel As Label
+    Friend WithEvents DailySpecialsLabel As Label
 End Class
--- Diner Menu/DinerMenuForm.vb
+++ Diner Menu/DinerMenuForm.vb
@@ -1,3 +1,8 @@
-﻿Public Class DinerMenuForm
+﻿'Zachary Christensen
+'RCET 2265
+'Fall 2023
+'Diner Menu
+'https://github.com/Minidude140/DinerMenu.git
+Public Class DinerMenuForm
 
 End Class
