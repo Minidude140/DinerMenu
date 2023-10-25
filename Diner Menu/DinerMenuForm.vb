ad50ee788b68223d07891e484a4e2551b803f95e
Zac <zac.minidude140@Gmail.com>
2023-10-25 14:30:26 -0600

    Added Menu button click events and updated comments

--- Diner Menu/DinerMenuForm.vb
+++ Diner Menu/DinerMenuForm.vb
@@ -5,18 +5,34 @@
 'https://github.com/Minidude140/DinerMenu.git
 Public Class DinerMenuForm
     'Custom Methods
+    ''' <summary>
+    ''' Updates DailySpecialLabel text to the welcome screen
+    ''' </summary>
     Sub SetDefault()
         DailySpecialsLabel.Text = "Welcome"
     End Sub
+
+    ''' <summary>
+    ''' Updates DailySpecialLabel text to the Soup menu
+    ''' </summary>
     Sub DisplaySoup()
         DailySpecialsLabel.Text = "Soup"
     End Sub
+
+    ''' <summary>
+    ''' Updates DailySpecialLabel text to the Salad menu
+    ''' </summary>
     Sub DisplaySalad()
         DailySpecialsLabel.Text = "Salad"
     End Sub
+
+    ''' <summary>
+    ''' Updates DailySpecialLabel text to the Fish menu
+    ''' </summary>
     Sub DisplayFish()
         DailySpecialsLabel.Text = "Fish"
     End Sub
+
     'Event Handlers
     Private Sub DinerMenuForm_Load(sender As Object, e As EventArgs) Handles Me.Load
         SetDefault()
@@ -25,4 +41,15 @@ Public Class DinerMenuForm
         Me.Close()
     End Sub
 
+    Private Sub SoupButton_Click(sender As Object, e As EventArgs) Handles SoupButton.Click
+        DisplaySoup()
+    End Sub
+
+    Private Sub SaladButton_Click(sender As Object, e As EventArgs) Handles SaladButton.Click
+        DisplaySalad()
+    End Sub
+
+    Private Sub FishButton_Click(sender As Object, e As EventArgs) Handles FishButton.Click
+        DisplayFish()
+    End Sub
 End Class
